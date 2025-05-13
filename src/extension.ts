07890c90049dd8ddb5e6429be39aa962e4ac5b8c
Finley Dabinett <finleytorterra@gmail.com>
2025-05-13 21:30:31 +0100

    index all php files in workspace folders

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let statusBarItem: vscode.StatusBarItem;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "phpdocwiki" is now active!');
+const findPhpFiles: () => Promise<string[]> = () => {
+	const phpFiles: string[] = [];
+	const workspaceFolders = vscode.workspace.workspaceFolders;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('phpdocwiki.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PhpDocWiki!');
+	return new Promise<string[]>((resolve, reject) => {
+		if (workspaceFolders) {
+			const promises = workspaceFolders.map(folder => vscode.workspace.findFiles(
+				new vscode.RelativePattern(folder, '**/*.php'),
+				new vscode.RelativePattern(folder, '**/vendor/**')
+			));
+
+			Promise.all(promises)
+				.then(groupedUris => {
+					groupedUris.forEach(uris => {
+						uris.forEach((uri) => {
+							phpFiles.push(uri.fsPath);
+						});
+					});
+					resolve(phpFiles);
+				});
+		} else {
+			resolve(phpFiles);
+		}
 	});
+};
 
-	context.subscriptions.push(disposable);
+export function activate(context: vscode.ExtensionContext) {
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.text = 'Indexing PHP files...';
+	statusBarItem.show();
+	findPhpFiles()
+		.then(files => {
+			statusBarItem.text = `PHP Files: ${files.length}`;
+			statusBarItem.tooltip = `Found ${files.length} PHP files for wiki`;
+		});
 }
 
 // This method is called when your extension is deactivated
